d293acec11eaccd04e8d049a5e74de12c6db6a80
Gabriel Souza de Oliveira <84600327+gabriel89067@users.noreply.github.com>
2026-01-08 14:17:48 -0300

    Update teste_motor_passo.cpp

--- teste_motor_passo.cpp
+++ teste_motor_passo.cpp
@@ -2,8 +2,11 @@
 
 #define STEP_PIN 3
 #define DIR_PIN 2
-#define TIME1 500
-int TIME = 2100;
+#define TIME1 45000
+int value = 1000;
+int count = 0;
+int TIME = 5500;
+int conf = 0;
 void setup() {
   pinMode(STEP_PIN, OUTPUT);
   pinMode(DIR_PIN, OUTPUT);
@@ -12,27 +15,45 @@ void setup() {
 
 void loop() {
   // Sentido 1
-  TIME = 2100;
+  TIME = 9400;
+  count = 0;
   digitalWrite(DIR_PIN, HIGH);
-  for (int i = 0; i < 200; i++) {
-     TIME = TIME - 15;
+  for (int i = 0; i < 125; i++) {
+     TIME = TIME - 40;
     digitalWrite(STEP_PIN, HIGH);
     delayMicroseconds(TIME);
     digitalWrite(STEP_PIN, LOW);
     delayMicroseconds(TIME);
+    
   }
+  while(count < value){
+  digitalWrite(STEP_PIN, HIGH);
+    delayMicroseconds(TIME);
+    digitalWrite(STEP_PIN, LOW);
+    delayMicroseconds(TIME);}
 
+
+count = 0;
+ 
   delay(100);
-TIME = 2100;
+TIME = 5500;
   // Sentido oposto
   digitalWrite(DIR_PIN, LOW);
-  for (int i = 0; i < 200; i++) {
-    TIME = TIME - 15;
+  for (int i = 0; i < 500; i++) {
+    TIME = TIME - 10;
     digitalWrite(STEP_PIN, HIGH);
     delayMicroseconds(TIME);
     digitalWrite(STEP_PIN, LOW);
     delayMicroseconds(TIME);
   }
 
+ while(count < value){
+  digitalWrite(STEP_PIN, HIGH);
+    delayMicroseconds(TIME);
+    digitalWrite(STEP_PIN, LOW);
+    delayMicroseconds(TIME);}
+
+
   delay(100);
+  
 }
